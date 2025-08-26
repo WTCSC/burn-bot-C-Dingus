8ca2797a5d2d2fac021c5e9db56a601a2dad0717
C-Dingus <rickardc@CSGP.EDU>
2025-08-26 10:19:07 -0600

    oh

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+NEGATIVE = ["no", "nah", "nope", "not really"];
+POSITIVE = ["yes", "yea", "ye", "sure", "i guess"];
+
 #get input from user
 def ask_for_data():
     print("what is your name, age, purpose in this world, and your hobby?");
     user_input = input();
-    return user_input.split(', ');
+    return user_input.lower().split(', ');
 
 #gets the users personal information
 while True:
@@ -13,5 +16,23 @@ while True:
     else:
         break;
 
+data = {
+    'name':words[0],
+    'age':words[1],
+    'purpose':words[2],
+    'hobby':words[3]
+}
 
-print(words);
+if int(data['age']) <= 10:
+    print(format("yea i can see that"));
+elif int(data['age']) >= 25:
+    print("someone as old as you should act a bit more mature dont you think?")
+else:
+    print(format("so your{data['age']} hmmm is your humor terrible?"));
+    i = input();
+    if i.lower() in NEGATIVE:
+        print("sure pal");
+    elif i.lower() in POSITIVE:
+        print("yea thats what i expected")
+    else:
+        print("what? ehh whatevere");
